3d2e1ac3a6a7704381d7aa1b255a256ebe735f4f
DrkCde15 <jcesarsantana215@gmail.com>
2026-07-07 11:20:12 -0300

    fix: redefine gunicorn worker settings and restore default values

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,10 +1,14 @@
 import os
 
-workers = int(os.getenv("GUNICORN_WORKERS", "1"))
+workers = 1
+worker_class = "gthread"
+threads = 4
+
 preload_app = False
 
 timeout = 300
 keepalive = 5
+
 max_requests = 1000
 max_requests_jitter = 100
 
